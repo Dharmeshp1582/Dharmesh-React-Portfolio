f033c8e45d96d93d8b7d621b21e3a8851b7d2f5c
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-19 16:15:45 +0530

    custom cursor & pointer add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
@@ -7,16 +7,56 @@ import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import BlurBlob from './components/BlurBlob';
+import BlurBlob from "./components/BlurBlob";
 
 const App = () => {
+  //for custom cursor
+  const outlineRef = useRef(null);
+  const dotRef = useRef(null);
+
+  //refs for custom position tracking
+  const mouse = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
+useEffect(() => {
+    const handleMouseMove = (e) => {
+      mouse.current.x = e.clientX;
+      mouse.current.y = e.clientY;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.1;
+      position.current.y += (mouse.current.y - position.current.y) * 0.1;
+
+      if (dotRef.current && outlineRef.current) {
+        dotRef.current.style.transform = `translate3d(${
+          mouse.current.x - 6
+        }px,${mouse.current.y - 6}px,0 )`;
+        outlineRef.current.style.transform = `translate3d(${
+          position.current.x - 20
+        }px,${position.current.y - 20}px,0 )`;
+      }
+
+      requestAnimationFrame(animate);
+    };
+    animate();
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="bg-[#050414]">
-
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+      <BlurBlob
+        position={{ top: "35%", left: "20%" }}
+        size={{ width: "30%", height: "40%" }}
+      />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-      
+
       <div className="relative pt-20">
         <Navbar />
         <About />
@@ -26,8 +66,20 @@ const App = () => {
         <Education />
         <Contact />
         <Footer />
-      </div>
 
+        {/* Custom cursor Ring  */}
+        <div
+          ref={outlineRef}
+          className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+          style={{ transition: "transform 0.1s ease-out" }}
+        ></div>
+
+        {/* Custom cursor dot  */}
+        <div
+          ref={dotRef}
+          className="fixed top-0 left-0 h-3 w-3 rounded-full bg-[#8245ec] pointer-events-none z-[9999]"
+        ></div>
+      </div>
     </div>
   );
 };
